c259d81ba30762938a1dc2e102e58714cebe59e3
zafarzaeem12 <106338772+zafarzaeem12@users.noreply.github.com>
2023-07-05 02:23:30 +0500

    connected to database

--- server.js
+++ server.js
@@ -12,8 +12,13 @@ app.use('/users/' , UserRoute )
 dotenv.config();
 
 
+const Mongo_Db_Url = 
+`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.yl84chg.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
-
+mongoose
+.connect(Mongo_Db_Url)
+.then((res) => console.log(`Database Connected Successfully`))
+.catch((err) => console.log(`Database Not Connected`))
 
 
 app.listen( process.env.PORT , () => {
