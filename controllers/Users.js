9799778440fcfef14c7daa01c96cac396db6faa3
zafarzaeem12 <106338772+zafarzaeem12@users.noreply.github.com>
2023-07-05 02:42:26 +0500

    registration api made

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,23 +1,44 @@
+const UserModel = require('../models/Users');
+const Crypto = require('crypto-js');
 
-const New_Register_User = (req,res,next) => {
+const New_Register_User = async (req, res, next) => {
+    try {
+        const data = new UserModel({
+            name: req.body.name,
+            username: req.body.username,
+            email: req.body.email,
+            password: Crypto.AES.encrypt( req.body.password, process.env.SECRET_KEY).toString(),
+            mobile_no: req.body.mobile_no
+        })
+
+        const newUser = await data.save();
+
+        res.send({
+            message: `${newUser?.name} Profile Created Successfully`,
+            status: 201,
+            data: newUser
+        })
+
+    } catch (err) {
+
+    }
+}
+
+const Get_All_Users = (req, res, next) => {
 
 }
 
-const Get_All_Users = (req,res,next) => {
+
+const Get_Specfic_User = (req, res, next) => {
 
 }
 
-
-const Get_Specfic_User = (req,res,next) => {
+const Update_User = (req, res, next) => {
 
 }
 
-const Update_User = (req,res,next) => {
-    
-}
+const Delete_User = (req, res, next) => {
 
-const Delete_User = (req,res,next) => {
-    
 }
 
 module.exports = {
--- models/Users.js
+++ models/Users.js
@@ -8,6 +8,9 @@ const UserSchema = new mongoose.Schema({
     username: {
         type: String,
     },
+    mobile_no: {
+        type: String,
+    },
     email: {
         type: String,
         unique: true,
