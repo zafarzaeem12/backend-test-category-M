50ea1107ecd3654818de5f7dc4518ee39cb84e89
zafarzaeem12 <106338772+zafarzaeem12@users.noreply.github.com>
2023-07-05 01:48:19 +0500

    Completed First task

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 
+app.get('/' , (req,res) => {
+    const Fetched_Data = 'Welcome to the Node.js server!'
+    res.send({
+        message:'Data Fetched successfully',
+        status:200,
+        data : Fetched_Data
+    })
+})
 
-app.listen(() => {
-    console.log('Welcome to the Node.js server!')
+const PORT = 3000
+
+app.listen( PORT ,() => {
+    console.log(`Server is running on ${PORT} port`)
 })
