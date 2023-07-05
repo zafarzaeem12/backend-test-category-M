4f3dfdd0e3bf6f2a020c9500bb32acaba9c84f39
zafarzaeem12 <106338772+zafarzaeem12@users.noreply.github.com>
2023-07-06 00:26:12 +0500

    posted file reader instruction.txt file and task 2 completed

---
+++ middleware/readAsyncFile.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const readFileAsync = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(data);
+    });
+  });
+}
+
+module.exports = {
+  readFileAsync
+};
--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const app = express();
+const readFiles = require('./middleware/readAsyncFile')
 
 app.use(express.json());
 
-app.get('/' , (req,res) => {
-    const Fetched_Data = 'Welcome to the Node.js server!'
-    res.send({
-        message:'Data Fetched successfully',
-        status:200,
-        data : Fetched_Data
-    })
+
+app.get('/', (req, res) => {
+    readFiles.readFileAsync('./instruction.txt')
+        .then(response => {
+            res.send({
+                message: 'Data Fetched successfully',
+                status: 200,
+                data: response
+            })
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
 })
 
 const PORT = 3000
 
-app.listen( PORT ,() => {
+app.listen(PORT, () => {
     console.log(`Server is running on ${PORT} port`)
 })
